943fb4d3c417755befc34ddc4d3abd9fbdfcc29c
streed <sean@reed.pub>
2019-03-13 03:16:41 -0700

    Last state for the notebook

--- src/road.py
+++ src/road.py
@@ -22,7 +22,7 @@ class Road:
             left_fit, right_fit, out_image = self.find_lanes_sliding_window(image)
             self.lane = Lane(left_fit, right_fit)
         else:
-            left_fit, right_fit, out_image = self.find_lanes_sliding_window(image)
+            left_fit, right_fit, out_image = self.find_lanes_from_existing_lanes(image)
             if self.lane.validate_new_fit(left_fit, right_fit):
                 self.lane.update_fit(left_fit, right_fit)
             else:
@@ -38,10 +38,10 @@ class Road:
         nonzero_x = np.array(nonzero[1])
         nonzero_y = np.array(nonzero[0])
 
-        left_lane_inds = ((nonzerox > (left.project(nonzeroy) - self.poly_margin)) &
-                          (nonzerox < (left.project(nonzeroy) + self.poly_margin)))
-        right_lane_inds = ((nonzerox > (right.project(nonzeroy) - self.poly_margin)) &
-                           (nonzerox < (right.project(nonzeroy) + self.poly_margin)))
+        left_lane_inds = ((nonzero_x > (left.project(nonzero_y) - self.poly_margin)) &
+                          (nonzero_x < (left.project(nonzero_y) + self.poly_margin)))
+        right_lane_inds = ((nonzero_x > (right.project(nonzero_y) - self.poly_margin)) &
+                           (nonzero_x < (right.project(nonzero_y) + self.poly_margin)))
 
         left_x = nonzero_x[left_lane_inds]
         left_y = nonzero_y[left_lane_inds]
@@ -49,11 +49,31 @@ class Road:
         right_y = nonzero_y[right_lane_inds]
 
         out_image = np.dstack((image, image, image)) * 255
-        out_image[lefty, leftx] = [255, 0, 0]
-        out_image[righty, rightx] = [0, 0, 255]
+        out_image[left_y, left_x] = [255, 0, 0]
+        out_image[right_y, right_x] = [0, 0, 255]
 
-        left_fit = np.polyfit(lefty, leftx, 2)
-        right_fit = np.polyfit(righty, rightx, 2)
+        if len(left_x) > 0 and len(right_x) > 0:
+            left_fit = np.polyfit(left_y, left_x, 2)
+            right_fit = np.polyfit(right_y, right_x, 2)
+
+            plot_y = np.linspace(0, image.shape[0]-1, image.shape[0])
+            left_fit_x = left_fit[0]*plot_y**2 + left_fit[1]*plot_y + left_fit[2]
+            right_fit_x = right_fit[0]*plot_y**2 + right_fit[1]*plot_y + right_fit[2]
+
+
+            left_line_window1 = np.array([np.transpose(np.vstack([left_fit_x-self.poly_margin, plot_y]))])
+            left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fit_x+self.poly_margin,plot_y])))])
+            left_line_pts = np.hstack((left_line_window1, left_line_window2))
+            right_line_window1 = np.array([np.transpose(np.vstack([right_fit_x-self.poly_margin, plot_y]))])
+            right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fit_x+self.poly_margin,plot_y])))])
+            right_line_pts = np.hstack((right_line_window1, right_line_window2))
+            window_image = np.zeros_like(out_image)
+            cv2.fillPoly(window_image, np.int_([left_line_pts]), (0,255, 0))
+            cv2.fillPoly(window_image, np.int_([right_line_pts]), (0,255, 0))
+            out_image = cv2.addWeighted(out_image, 1, window_image, 0.3, 0)
+        else:
+            left_fit = [False, False, False]
+            right_fit = [False, False, False]
 
         return left_fit, right_fit, out_image
 
